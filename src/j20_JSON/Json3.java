756082407d0290cced6e4493edcd1998780c8aa2
DESKTOP-NUOPUIF\ITPS <heeheer_g@naver.com>
2023-01-16 16:35:50 +0900

    01.16 오후

---
+++ src/j20_JSON/Json3.java
@@ -0,0 +1,21 @@
+package j20_JSON;
+
+import com.google.gson.Gson;
+
+public class Json3 {
+
+	public static void main(String[] args) {
+
+		Gson gson = new Gson();
+//		System.out.println(Integer.valueOf("100") + 50);
+//		System.out.println(Integer.parseInt("100")+ 50);
+		
+		gson.toJson(null); //object -> json
+		gson.fromJson("", Object.class); // json -> object
+		
+		
+		
+
+	}
+
+}

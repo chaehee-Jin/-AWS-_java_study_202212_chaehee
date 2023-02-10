9b72a9258888db5d6b5fbb56fee0712bef01f3e5
DESKTOP-NUOPUIF\ITPS <heeheer_g@naver.com>
2023-02-10 13:53:29 +0900

    23.02.10 심플소켓수정 및 소스코드 추가

--- src/simplechatting/client/ChattingClient.java
+++ src/simplechatting/client/ChattingClient.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -23,6 +24,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class ChattingClient extends JFrame {
 
@@ -32,12 +35,11 @@ public class ChattingClient extends JFrame {
 	private JPanel contentPane;
 	private JTextField ipInput;
 	private JTextField portInput;
-	private JTextField usernameInput;
-	private 	JTextArea contentView;
+	private JTextArea contentView;
+	private JTextField messageInput;
+	private JList userList;
+	private DefaultListModel<String> userListModel;
 
-	
-	
-	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -61,11 +63,12 @@ public class ChattingClient extends JFrame {
 		contentPane.setLayout(null);
 
 		ipInput = new JTextField();
+		ipInput.setText("127.0.0.1");
 		ipInput.setBounds(312, 10, 106, 21);
 		contentPane.add(ipInput);
 		ipInput.setColumns(10);
 
-		JButton connectButton = new JButton("연결\r\n");
+		JButton connectButton = new JButton("연결");
 		connectButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -75,7 +78,7 @@ public class ChattingClient extends JFrame {
 				port = Integer.parseInt(portInput.getText());
 
 				try {
-					socket = new Socket(ip, port); //소켓 객체생성 , 클라이언트가 이 ip로 접속하겠다
+					socket = new Socket(ip, port); // 소켓 객체생성 , 클라이언트가 이 ip로 접속하겠다
 					JOptionPane.showMessageDialog(null, socket.getInetAddress() + "서버 접속", "접속성공",
 							JOptionPane.INFORMATION_MESSAGE);
 
@@ -85,13 +88,45 @@ public class ChattingClient extends JFrame {
 						username = JOptionPane.showInputDialog(null, "사용자이름을 입력하세요", JOptionPane.INFORMATION_MESSAGE);
 
 						OutputStream outputStream = socket.getOutputStream(); // 소켓이 서버에게 갈수있는 통로를 연다
-						PrintWriter out = new PrintWriter(outputStream, true); // 클라이언트가 서버에게 데이터를 줌 
-						out.println(username + "님이 접속하였습니다");
+						PrintWriter out = new PrintWriter(outputStream, true); // 클라이언트가 서버에게 데이터를 줌
+						out.println(username);
 
-						String welcomeMessage = in.readLine(); // 변수를 담음
-						contentView.append(welcomeMessage); // 웹컴 메세지를 뿌려줌 여러개면 모든 클라이언트에게 다 보내야함 
+//						String welcomeMessage = in.readLine(); // 변수를 담음
+//						contentView.append(welcomeMessage); // 웹컴 메세지를 뿌려줌 여러개면 모든 클라이언트에게 다 보내야함
 
 					}
+					Thread thread = new Thread(() -> {
+						try {
+							InputStream input = socket.getInputStream();
+							BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+							// 웰컴 메세지만 잘라서 보내줌
+							while (true) {
+								String message = reader.readLine();
+								if (message.startsWith("@welcome")) {
+									int tokenIndex = message.indexOf("/");
+									message = message.substring(tokenIndex + 1);
+									// 해당 문자열중에 해당 인덱스부터 문자열을 자르라는 의미
+								} else if (message.startsWith("@userList")) {
+									int tokenIndex = message.indexOf("/");
+									message = message.substring(tokenIndex + 1);
+									String[] usernames = message.split(",");
+									userListModel.clear();
+									for (String username : usernames) {
+										userListModel.addElement(username);
+									}
+									continue;
+								}
+								contentView.append(message + "\n");
+
+							}
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+
+					});
+
+					thread.start();
 
 				} catch (ConnectException e1) {
 					JOptionPane.showMessageDialog(null, "서버 접속 실패", "접속실패", JOptionPane.ERROR_MESSAGE);
@@ -106,6 +141,7 @@ public class ChattingClient extends JFrame {
 		contentPane.add(connectButton);
 
 		portInput = new JTextField();
+		portInput.setText("9090");
 		portInput.setBounds(420, 10, 52, 21);
 		contentPane.add(portInput);
 		portInput.setColumns(10);
@@ -117,20 +153,69 @@ public class ChattingClient extends JFrame {
 		contentView = new JTextArea();
 		contentScroll.setViewportView(contentView);
 
-		usernameInput = new JTextField();
-		usernameInput.setBounds(311, 41, 225, 200);
-		contentPane.add(usernameInput);
-		usernameInput.setColumns(10);
+		JScrollPane userListScroll = new JScrollPane();
+		userListScroll.setBounds(312, 41, 224, 200);
+		contentPane.add(userListScroll);
+
+		userListModel = new DefaultListModel<>();
+		userList = new JList(userListModel);
+		userListScroll.setViewportView(userList);
 
 		JScrollPane messageScroll = new JScrollPane();
 		messageScroll.setBounds(12, 254, 419, 68);
 		contentPane.add(messageScroll);
 
-		JTextArea messageInput = new JTextArea();
+		messageInput = new JTextField();
+		messageInput.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					if (!messageInput.getText().isBlank()) {
+						try {
+							OutputStream outputStream = socket.getOutputStream();
+							PrintWriter out = new PrintWriter(outputStream, true);
+
+							out.println(username + ":" + messageInput.getText());
+
+						} catch (IOException e1) {
+							e1.printStackTrace();
+
+						}
+					}
+
+				}
+			}
+		});
 		messageScroll.setViewportView(messageInput);
 
-		JButton sendButton = new JButton("전송\r\n");
+		JButton sendButton = new JButton("전송");
+		sendButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (!messageInput.getText().isBlank()) {
+					try {
+						OutputStream outputStream = socket.getOutputStream();
+						PrintWriter out = new PrintWriter(outputStream, true);
+
+						out.println(username + ":" + messageInput.getText());
+						messageInput.setText("");
+//						InputStream inputStream = socket.getInputStream();
+//						BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+//
+//						String message = in.readLine();
+//						contentView.append("\n" + message);
+//						messageInput.setText("");
+
+					} catch (IOException e1) {
+						e1.printStackTrace();
+
+					}
+				}
+			}
+		});
 		sendButton.setBounds(439, 254, 97, 68);
 		contentPane.add(sendButton);
+
 	}
 }
--- src/simplechatting/server/ServerApplication.java
+++ src/simplechatting/server/ServerApplication.java
@@ -8,55 +8,110 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketOption;
+import java.net.SocketOptions;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.security.auth.login.AppConfigurationEntry;
 
+import lombok.Data;
 
+@Data
+class ConnectedSocket extends Thread {
+	private static List<ConnectedSocket> socketList = new ArrayList<>();
+	private Socket socket;
+	private InputStream inputStream;
+	private OutputStream outputStream;
+
+	private String username;
+
+	public ConnectedSocket(Socket socket) {
+		this.socket = socket;
+		socketList.add(this);
+	}
+
+	@Override
+	public void run() {
+		try {
+			outputStream = socket.getOutputStream();
+			PrintWriter out = new PrintWriter(outputStream, true); // 클라이언트에 줄수 있는 통로를 하나 열어둠
+			out.println("join"); // 전송(join 이라는 텍스트 값을 )
+
+			inputStream = socket.getInputStream(); // 받는 녀석 서버로 부터 데이터를 받는 통로를 연다
+			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+
+			username = in.readLine(); // 방금 클라인언트가 '님이 접속하였습니다' 값을 보냄
+			System.out.println(username + "님이 접속하였습니다"); // 콘솔창 출력
+
+			String userListStr = "";
+
+			for (int i = 0; i < socketList.size(); i++) {
+				userListStr += socketList.get(i).getUsername();
+				if (i < socketList.size() - 1) {
+					userListStr += ",";
+
+				}
+			}
+
+			for (ConnectedSocket connectedSocket : socketList) {
+				outputStream = connectedSocket.getSocket().getOutputStream();
+				out = new PrintWriter(outputStream, true);
+				out.println("@welcome/" + username + "님이 접속하였습니다");
+				out.println("@userList/" + userListStr);
+			}
+
+			while (true) {
+				String message = in.readLine();
+				for (ConnectedSocket connectedSocket : socketList) {
+					outputStream = connectedSocket.getSocket().getOutputStream();
+					out = new PrintWriter(outputStream, true);
+					out.println(message);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+
+	}
+}
 
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
-
 		try {
-			serverSocket = new ServerSocket(9090); //서버소켓 하나생성 
+			serverSocket = new ServerSocket(9090); // 서버소켓 하나생성
 			System.out.println("===<<<서버 실행>>>===");
-			// while(true) {
-			Socket socket = serverSocket.accept(); // 클라이언트의 접속을 기다리는 녀석
-			// 연결버튼을 누를 때 까지 기다림
-			// System.out.println(socket.getInetAddress() + "클라이언트가 연결되었습니다");
-			OutputStream outputStream = socket.getOutputStream();
-			PrintWriter out = new PrintWriter(outputStream, true); // 클라이언트에 줄수 있는 통로를 하나 열어둠 
-			out.println("join"); // 전송(join 이라는 텍스트 값을 )
-
-			InputStream inputStream = socket.getInputStream(); // 받는 녀석 서버로 부터 데이터를 받는 통로를 연다
-			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-			
-			String welcomeMessage = in.readLine(); // 방금 클라인언트가 '님이 접속하였습니다' 값을 보냄 
-			System.out.println(welcomeMessage); // 콘솔창 출력
-			out.println(welcomeMessage); // 서버가 클라이언트에게 웰컴 메세지를 보냄 
+			List<Socket> socketList = new ArrayList<>();
 
 			while (true) {
-				in.readLine();
+				Socket socket = serverSocket.accept(); // 클라이언트의 접속을 기다리는 녀석
+
+				ConnectedSocket connectedSocket = new ConnectedSocket(socket);
+
+				connectedSocket.start();
+				// 연결버튼을 누를 때 까지 기다림
+				// System.out.println(socket.getInetAddress() + "클라이언트가 연결되었습니다");
+				// socketList.add(socket);
+
+				// Thread thread = new Thread(() -> {
 
 			}
-
-			// }
 		} catch (IOException e) {
-
 			e.printStackTrace();
 		} finally {
 			if (serverSocket != null) {
 				try {
 					serverSocket.close();
 				} catch (IOException e) {
-
 					e.printStackTrace();
+
 				}
 			}
 			System.out.println("===<<<서버 종료>>>===");
-
 		}
-
 	}
 
 }
